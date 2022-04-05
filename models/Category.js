36cdf1f1e87f27b6e102b364e478a431bef1f990
Bemourn <70115233+koenib@users.noreply.github.com>
2022-04-04 21:50:39 -0600

    create models for db structure

--- models/Category.js
+++ models/Category.js
@@ -1,1 +1,25 @@
-// categories for products
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'category',
+});
+
+module.exports = Category;
